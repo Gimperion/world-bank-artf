935e78409a877c6f380a57f0c09552f8b3f8da80
Lou Huang <lou.huang@socrata.com>
2014-06-10 18:29:57 -0400

    Add SCSS workflow and a reasonable .gitignore

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+var autoprefix    = require('gulp-autoprefixer'),
+    cssimport     = require('gulp-cssimport'),
+    debug         = require('gulp-debug'),
+    entityconvert = require('gulp-entity-convert'),
+    livereload    = require('gulp-livereload'),
+    minifyCSS     = require('gulp-minify-css'),
+    sass          = require('gulp-sass');
+
+gulp.task('css', function () {
+    gulp.src('./stylesheets/styles.scss')
+        .pipe(sass(), { errLogToConsole: true })
+        .pipe(autoprefix('last 2 versions', 'Explorer > 8'))
+        .pipe(cssimport())
+        .pipe(entityconvert({ type: 'css' }))
+        .pipe(minifyCSS({ keepSpecialComments: 0 }))
+        .pipe(debug({ verbose: false }))
+        .pipe(gulp.dest('./css'));
+});
