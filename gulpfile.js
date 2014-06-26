de750f64657b60db1514db4de79ca6ebcf3f33a4
Lou Huang <lou@codeforamerica.org>
2014-06-26 17:01:00 -0400

    Convert 4 spaces to 2

--- gulpfile.js
+++ gulpfile.js
@@ -9,12 +9,12 @@ var autoprefix    = require('gulp-autoprefixer'),
     sass          = require('gulp-sass');
 
 gulp.task('css', function () {
-    gulp.src('./stylesheets/styles.scss')
-        .pipe(sass(), { errLogToConsole: true })
-        .pipe(autoprefix('last 2 versions', 'Explorer > 7'))
-        .pipe(cssimport())
-        .pipe(entityconvert({ type: 'css' }))
-        .pipe(minifyCSS({ keepSpecialComments: 0 }))
-        .pipe(debug({ verbose: false }))
-        .pipe(gulp.dest('./css'));
+  gulp.src('./stylesheets/styles.scss')
+    .pipe(sass(), { errLogToConsole: true })
+    .pipe(autoprefix('last 2 versions', 'Explorer > 7'))
+    .pipe(cssimport())
+    .pipe(entityconvert({ type: 'css' }))
+    .pipe(minifyCSS({ keepSpecialComments: 0 }))
+    .pipe(debug({ verbose: false }))
+    .pipe(gulp.dest('./css'));
 });
